388cb7b63c3bd5ae33aab9aabfc21f0af95d903e
gpranavnisan <gpranavnisan@gmail.com>
2026-04-08 21:35:53 +0530

    Update nisan.py

--- nisan.py
+++ nisan.py
@@ -1,4 +1,4 @@
-#take two number from the user and perform:
+#1.take two number from the user and perform:
 a=int(input("enter a number:"))
 b=int(input("enter a number:"))
 print("sum:",a+b)
@@ -9,23 +9,23 @@ print("remainder:",a%b)
 print("floor division:",a//b)
 print("power:",a**b)
 
-#program to calculate area and perimeter of rectangle:
+#2.1.program to calculate area and perimeter of rectangle:
 l=int(input("enter a value of length:",))
 b=int(input("enter a value of breadth:",))
 a=print("area of rectangle:",l*b)
 p=print("perimeter of rectangle:",2*l+2*b)
 
-#program to calculate area and perimeter of square:
+#2.2.program to calculate area and perimeter of square:
 s=int(input("enter a value of side:",))
 print("area of square:",s*s)
 print("perimeter of square:",4*s)
 
-#program to calculate area and circumference of circle:
+#2.3.program to calculate area and circumference of circle:
 r=int(input("enter value of radius:",))
 print("area of circle:",3.14*r**2)
 print("circumference of circle:",2*3.14*r)
 
-#average of therr numbers:
+#3.average of therr numbers:
 a=int(input("enter a number:",))
 b=int(input("enter a number:",))
 c=int(input("enter a number:",))
@@ -33,19 +33,31 @@ d=a+b+c
 e=d//2
 print("average:",e)
 
-#programs to check
-#both numbers are equal
+#4.programs to check
+#4.1.both numbers are equal
 a=int(input("enter a number:",))
 b=int(input("enter a number:",))
 if a==b:
     print("equal")
 else:
     print("not equal")
-#a number is greater than other number:
+#4.2.a number is greater than other number:
 a=int(input("enter a number:",))
 b=int(input("enter a number:",))
 if b<a:
     print("a is greater than b")
 else:
     print("b is greater than a")
-    
+#4.3.to find the number is less than or equal to another number:
+a=int(input("enter a number:",))
+b=int(input("enter another number;",))
+if a<b:
+    print("a is less than b")
+elif a==b:
+    print("the numbers are equal")
+else:
+    print("b is less than a")
+
+#5.to find the square root of a number:
+a=int(input("enter a number:",))
+print("the square root of the number is:",a**0.5)
